3dadc776c55fd9bdaa48247d394a577b687b05d0
lanrelegend-tech <Lanrelegend@gmail.com>
2026-09-20 00:49:09 +0100

    Prepare Django backend for deployment

--- config/settings.py
+++ config/settings.py
@@ -106,13 +106,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
