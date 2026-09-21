37fa92bf1459ea0cd6819939f12e96ace93262b5
lanrelegend-tech <Lanrelegend@gmail.com>
2026-09-21 15:07:04 +0100

    Fix payment callback CORS

--- config/settings.py
+++ config/settings.py
@@ -212,7 +212,18 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
-CORS_ALLOW_HEADERS = list(default_headers) + [
+
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
     "x-guest-session-id",
 ]
 CORS_ALLOW_CREDENTIALS = True
