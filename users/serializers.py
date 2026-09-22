149e5da481246bbbad8f942f189c801a48b0a2ba
lanrelegend-tech <Lanrelegend@gmail.com>
2026-09-22 02:30:10 +0100

    user sterilizer done

--- users/serializers.py
+++ users/serializers.py
@@ -10,17 +10,33 @@ class RegisterSerializer(serializers.ModelSerializer):
         min_length=8
     )
 
+    password2 = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
     class Meta:
         model = User
         fields = [
             "username",
             "email",
             "password",
+            "password2",
             "first_name",
             "last_name",
         ]
 
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise serializers.ValidationError({
+                "password2": "Passwords do not match."
+            })
+
+        return attrs
+
     def create(self, validated_data):
+        validated_data.pop("password2")
+
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
